83926cdc4b34520f76d9ff5474d5593e7af6cb29
wagerengineering-ops <wagerengineering@gmail.com>
2026-01-03 16:04:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('btnLogin').onclick=()=>{
+document.getElementById('screen-login').classList.remove('active');
+document.getElementById('screen-executive-dashboard').classList.add('active');
+document.getElementById('topTitle').textContent='Executive Dashboard';
+};
